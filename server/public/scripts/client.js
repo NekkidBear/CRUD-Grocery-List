9ae55f593834d36f1980b3434f6a1bc67b5711d0
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-04-16 22:33:30 -0500

    update html with form inputs and buttons

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,44 +1,63 @@
-console.log('👋🌍');
+console.log("👋🌍");
 
 /** this function gets data from the server */
-function getGroceries(){
-    axios({
-        method: "GET",
-        url: "/groceries"
-    })
+function getGroceries() {
+  axios({
+    method: "GET",
+    url: "/groceries",
+  })
     .then((response) => {
-        let listData = response.data;
-        renderDOM(listData);
+      let listData = response.data;
+      renderDOM(listData);
     })
     .catch((error) => {
-        console.log("There was an error retrieving your list. Error: ", error)
-    }
-)}
+      console.log("There was an error retrieving your list. Error: ", error);
+    });
+}
 
-function renderDOM(data){
-    let groceryList = data;
+function renderDOM(data) {
+  let groceryList = data;
+  console.log("groceryList is:", groceryList);
+  if (groceryList) {
     let tableBodyLoc = document.getElementById("tableBodylocation");
-    let runningTotal = 0
-    tableBodyLoc.innerHTML = ""
+    let runningTotal = 0;
+    tableBodyLoc.innerHTML = "";
 
-    for (let row of groceryList){
-        let unitPrice = Number(row.unit_price);
-        let quantity = Number(row.quantity);
-        let extendedPrice = unitPrice * quantity;
-        runningTotal += extendedPrice;
+    for (let row of groceryList) {
+      let unitPrice = Number(row.unit_price.replace("$", ""));
+      let quantity = Number(row.quantity);
+      console.log("Type of Unit Price:", typeof row.unit_price);
+      console.log("type of Quantity: ", typeof row.quantity);
 
-        tableBodyLoc.innerHTML += `
+      let extendedPrice = unitPrice * quantity;
+      runningTotal += extendedPrice;
+      let extendedPriceFormatted = extendedPrice.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
+      });
+      let runningTotalFormatted = runningTotal.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
+      });
+
+      tableBodyLoc.innerHTML += `
         <tr>
             <td>${row.item}</td>
             <td>${row.unit_price}</td>
             <td>${row.quantity}</td>
             <td>${row.in_cart}</td>
-            <td>${extendedPrice}</td>
-            <td>${runningTotal}</td>
+            <td>${extendedPriceFormatted}</td>
+            <td>${runningTotalFormatted}</td>
+            <td>
+                <button id="checkMarkItem" onclick="toggleInCart(${row.id})">✅</button>
+            </td>
+            <td>
+                <button id="deleteItem" onclick = "deleteItem(${row.id})">❌</button>
+            </td>
         </tr>
         `;
     }
+  }
 }
 
 getGroceries();
-renderDOM();
